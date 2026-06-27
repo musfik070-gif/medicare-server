77e6643732b895e827f286fe3a1fc809ec48be92
musfik070-gif <musfik070@gmail.com>
2026-06-28 02:05:03 +0600

    new routes added

--- routes/appointmentRoutes.js
+++ routes/appointmentRoutes.js
@@ -4,10 +4,12 @@ const {
   getAllAppointmentsAdmin,
   getDoctorAppointments,
   updateAppointmentStatus,
+  getPatientAppointments,
 } = require("../controllers/appointmentController");
 const verifyToken = require("../middleware/verifyToken");
 const verifyAdmin = require("../middleware/verifyAdmin");
 const verifyDoctor = require("../middleware/verifyDoctor");
+const verifyPatient = require("../middleware/verifyPatient");
 
 // Admin Routes
 router.get("/admin/all", verifyToken, verifyAdmin, getAllAppointmentsAdmin);
@@ -24,6 +26,14 @@ router.patch(
   verifyToken,
   verifyDoctor,
   updateAppointmentStatus,
+);
+
+// Patient Routes
+router.get(
+  "/patient/my-appointments",
+  verifyToken,
+  verifyPatient,
+  getPatientAppointments,
 );
 
 module.exports = router;
