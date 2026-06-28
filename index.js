6b5a3e7196585f7f3b7e0b178570710afc6394eb
musfik070-gif <musfik070@gmail.com>
2026-06-29 05:43:24 +0600

    "fix production cors"

--- index.js
+++ index.js
@@ -14,9 +14,20 @@ const reviewRoutes = require("./routes/reviewRoutes");
 
 const app = express();
 
+const allowedOrigins = [
+  "http://localhost:3000",
+  process.env.CLIENT_URL,
+].filter(Boolean);
+
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   }),
 );
